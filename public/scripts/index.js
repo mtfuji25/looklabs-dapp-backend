d6146f49067cf831bfde5fad56b8a190fb6902c6
JoaoMario109 <joao.mario@expertise.dev>
2021-09-26 16:31:43 -0300

    Fixed the resize method and added handles to events

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,65 @@
+//
+// Assets directories
+//
+
+const ICON_DIR = "../assets/icons/";
+const IMAGE_DIR = "../assets/images/";
 const SPRITE_DIR = "../assets/sprites/";
+
+// Constants
+const ROOT_DIV = document.querySelector("#root");
+
+// In game used key codes
+
+const KEY_CODE = {
+    KEY_W: 87,
+    KEY_S: 83,
+    KEY_A: 65,
+    KEY_D: 68,
+    KEY_SPACE: 32,
+};
+
+//
+//  Handled by PIXI
+//
+const handlePointerMove = (event) => {
+    
+}
+
+//
+// Handled by default browser event system
+//
+
+const handleKeyUp = (event) => {
+    console.log(event);
+};
+
+const handleKeyDown = (event) => {
+    console.log(event);
+};
+
+const handleMouseBtnDown = (event) => {
+    console.log(event);
+};
+
+const handleMouseBtnUp = (event) => {
+    console.log(event);
+};
+
+const handleMouseWheel = (event) => {
+    let up = (event.wheelDelta > 0) ? true : false;
+
+    up ? console.log("is up") : console.log("is down");
+};
 
 window.onload = () => {
     let app = new PIXI.Application({
-        width: innerWidth - 10,
-        height: innerHeight - 10,
         resolution: devicePixelRatio,
-        backgroundColor: 0xAAAAAA
+        backgroundColor: 0xAAAAAA,
+        resizeTo: ROOT_DIV
     });
 
-    document.querySelector("#root").appendChild(app.view);
+    ROOT_DIV.appendChild(app.view);
 
     let player = new PIXI.Sprite.from(SPRITE_DIR + "mage.png");
     player.anchor.set(0.5);
@@ -17,13 +68,16 @@ window.onload = () => {
 
     app.stage.addChild(player);
 
+    // Allow the interactive mode of PIXI
     app.stage.interactive = true;
 
-    app.stage.on("pointermove", (e) => {
-        let pos = e.data.global;
+    // Set the PIXI event listener
+    app.stage.on("pointermove", handlePointerMove);
 
-        player.x = pos.x;
-        player.y = pos.y;
-    })
-
+    // Set the browser event listeners
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    window.addEventListener("mousedown", handleMouseBtnDown);
+    window.addEventListener("mouseup", handleMouseBtnUp);
+    window.addEventListener("wheel", handleMouseWheel);
 };
