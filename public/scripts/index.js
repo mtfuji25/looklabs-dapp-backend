515d6e0fed3c2f5c6b8c71e4ab5ca7ca431bb6f3
JoaoMario109 <joao.mario@expertise.dev>
2021-09-26 15:25:46 -0300

    Added the base canvas and favicon to the project

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,10 @@
-alert("Hello World");
+window.onload = () => {
+    let app = new PIXI.Application({
+        width: innerWidth - 10,
+        height: innerHeight - 10,
+        resolution: devicePixelRatio,
+        backgroundColor: 0xAAAAAA
+    });
+
+    document.querySelector("#root").appendChild(app.view);
+};
