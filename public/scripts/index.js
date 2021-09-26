8dd39a285495d8417ce650ac7acd374976a58d9b
JoaoMario109 <joao.mario@expertise.dev>
2021-09-26 15:38:43 -0300

    Basic sprite loading and cursor movement

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,5 @@
+const SPRITE_DIR = "../assets/sprites/";
+
 window.onload = () => {
     let app = new PIXI.Application({
         width: innerWidth - 10,
@@ -7,4 +9,21 @@ window.onload = () => {
     });
 
     document.querySelector("#root").appendChild(app.view);
+
+    let player = new PIXI.Sprite.from(SPRITE_DIR + "mage.png");
+    player.anchor.set(0.5);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+
+    app.stage.addChild(player);
+
+    app.stage.interactive = true;
+
+    app.stage.on("pointermove", (e) => {
+        let pos = e.data.global;
+
+        player.x = pos.x;
+        player.y = pos.y;
+    })
+
 };
