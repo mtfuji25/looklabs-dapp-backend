f638db4a7e5e62eb640d991524eaada4149f638a
JoaoMario109 <joao.mario@expertise.dev>
2021-09-26 17:10:00 -0300

    Added base handles and game loop to the game

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,23 +1,39 @@
 //
-// Assets directories
+//  Assets directories
 //
 
 const ICON_DIR = "../assets/icons/";
 const IMAGE_DIR = "../assets/images/";
 const SPRITE_DIR = "../assets/sprites/";
 
-// Constants
+//  Constants
 const ROOT_DIV = document.querySelector("#root");
 
-// In game used key codes
+//  In game used key codes
 
 const KEY_CODE = {
     KEY_W: 87,
     KEY_S: 83,
     KEY_A: 65,
     KEY_D: 68,
-    KEY_SPACE: 32,
+    KEY_ESC: 27,
+    KEY_SPACE: 32
 };
+
+const MOUSE_CODE = {
+    MOUSE_LEFT: 0,
+    MOUSE_RIGTH: 2,
+    MOUSE_MIDDLE: 1
+};
+
+//
+//  Data containers
+//
+
+//  Represent the current state of the key
+//  true == down  /  false == up
+let keyState = {};
+let btnState = {};
 
 //
 //  Handled by PIXI
@@ -27,31 +43,37 @@ const handlePointerMove = (event) => {
 }
 
 //
-// Handled by default browser event system
+//  Handled by default browser event system
 //
 
-const handleKeyUp = (event) => {
-    console.log(event);
-};
-
+//  When some key is pressed down
 const handleKeyDown = (event) => {
-    console.log(event);
+    keyState[event.keyCode] = true;
 };
 
+//  When some key is released up
+const handleKeyUp = (event) => {
+    keyState[event.keyCode] = false;
+};
+
+//  When some button is pressed down
 const handleMouseBtnDown = (event) => {
-    console.log(event);
+    btnState[event.button] = true;
 };
 
+//  When some button is released up
 const handleMouseBtnUp = (event) => {
-    console.log(event);
+    btnState[event.button] = false;
 };
 
+//  When mouse wheel is activated
 const handleMouseWheel = (event) => {
     let up = (event.wheelDelta > 0) ? true : false;
 
-    up ? console.log("is up") : console.log("is down");
+    
 };
 
+//  Software entry point
 window.onload = () => {
     let app = new PIXI.Application({
         resolution: devicePixelRatio,
@@ -74,10 +96,27 @@ window.onload = () => {
     // Set the PIXI event listener
     app.stage.on("pointermove", handlePointerMove);
 
+    // Remove the default behavior of browser of open contextmenu
+    // when right mouse button is pressed on page
+    document.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+    }, false);
+
     // Set the browser event listeners
     window.addEventListener("keydown", handleKeyDown);
     window.addEventListener("keyup", handleKeyUp);
     window.addEventListener("mousedown", handleMouseBtnDown);
     window.addEventListener("mouseup", handleMouseBtnUp);
     window.addEventListener("wheel", handleMouseWheel);
+
+    app.ticker.add(gameLoop);
 };
+
+//
+//  Main game loop
+//
+
+const gameLoop = () => {
+    console.log(keyState);
+    console.log(btnState);
+}
