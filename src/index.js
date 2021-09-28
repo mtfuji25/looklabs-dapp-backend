fd6cbfc0d5bed5552fb57be431340d8529a76aa9
JoaoMario109 <joao.mario@expertise.dev>
2021-09-28 15:40:13 -0300

    changed gitignore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("Hello");
+import { Application } from "@pixi/app";
+
+const $ = (name) => {
+    return document.querySelector(name);
+}
+
+const ROOT = $("#root");
+
+const main = () => {
+    
+    // Create a PIXI application
+    let app = new Application({
+        resolution: devicePixelRatio,
+        backgroundColor: 0xAAAAAA,
+        resizeTo: ROOT
+    });
+
+    ROOT.appendChild(app.view);
+}
+
+// Call the main function and start the app.
+main();
