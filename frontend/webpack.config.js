668648c06c9148f8d8df0062fd57eaca705cf9ac
Leo Mercier <leo@crowdform.co.uk>
2021-11-11 12:28:08 +0000

    seperate webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
-
-  // For development purpose only
-  watch: true,
-  mode: "development",
-  // ! For development purpose only
+  entry: "./src/index.ts",
 
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'engine.js',
-    path: path.resolve(__dirname, 'public'),
+    filename: "engine.js",
+    path: path.resolve(__dirname, "public"),
   },
 };
---
+++ frontend/webpack.development.config.js
@@ -0,0 +1,9 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.js");
+
+module.exports = merge(common, {
+  // For development purpose only
+  watch: true,
+  mode: "development",
+  // ! For development purpose only
+});
