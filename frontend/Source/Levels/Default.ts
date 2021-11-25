dedd48528b22d7f60c23334a5f25f3d25fba2f97
JoaoMario109 <joao.mario@expertise.dev>
2021-11-25 18:12:04 -0300

    Returned all levels check

--- frontend/Source/Levels/Default.ts
+++ frontend/Source/Levels/Default.ts
@@ -13,29 +13,22 @@ import { ResultsLevel } from "./Results";
 class DefaultLevel extends Level {
 
     onStart(): void {
-        // this.context.ws.request({
-        //     uuid: uuidv4(),
-        //     type: "request",
-        //     content: {
-        //         type: "game-status"
-        //     }
-        // })
-        // .then((response) => {
-        //     const content = response.content as GameStatus;
-        //     console.log(response)
-        //     this.startLevels(content);
-        // })
-        // .catch((err) => {
-        //     console.log(err);
-        //     this.context.close = true;
-        // });
-        this.context.engine.loadLevel(new ResultsLevel(
-            this.context, "Results",
-            {
-                gameId: 12
+        this.context.ws.request({
+            uuid: uuidv4(),
+            type: "request",
+            content: {
+                type: "game-status"
             }
-        ));
-    
+        })
+        .then((response) => {
+            const content = response.content as GameStatus;
+            console.log(response)
+            this.startLevels(content);
+        })
+        .catch((err) => {
+            console.log(err);
+            this.context.close = true;
+        });
     }
 
     startLevels(response : GameStatus) {
