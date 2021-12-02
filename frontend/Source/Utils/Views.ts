0c8001d8fabb3a23cda327c28148b9697f7dad57
JoaoMario109 <joao.mario@expertise.dev>
2021-12-02 13:44:07 -0300

    Modified view conversions to use bi-dimensional constants

--- frontend/Source/Utils/Views.ts
+++ frontend/Source/Utils/Views.ts
@@ -1,31 +1,31 @@
-import { CONTAINER_DIM } from "../Constants/Constants";
+import { CONTAINER_DIM_X, CONTAINER_DIM_Y } from "../Constants/Constants";
 import { Vec2 } from "./Math";
 
 const wordToView = (pos: Vec2) => {
     return new Vec2(
-        (pos.x * CONTAINER_DIM + CONTAINER_DIM) / 2.0,
-        CONTAINER_DIM - (pos.y * CONTAINER_DIM + CONTAINER_DIM) / 2.0
+        (pos.x * CONTAINER_DIM_X + CONTAINER_DIM_X) / 2.0,
+        CONTAINER_DIM_Y - (pos.y * CONTAINER_DIM_Y + CONTAINER_DIM_Y) / 2.0
     );
 }
 
 const viewToWord = (pos: Vec2) => {
     return new Vec2(
-        (pos.x / CONTAINER_DIM * 2.0) - 1.0,
-        ((CONTAINER_DIM - pos.y) / CONTAINER_DIM * 2.0) - 1.0
+        (pos.x / CONTAINER_DIM_X * 2.0) - 1.0,
+        ((CONTAINER_DIM_Y - pos.y) / CONTAINER_DIM_Y * 2.0) - 1.0
     );
 }
 
 const resToNdc = (res: Vec2) => {
     return new Vec2(
-        (res.x / CONTAINER_DIM) * 2.0,
-        (res.y / CONTAINER_DIM) * 2.0
+        (res.x / CONTAINER_DIM_X) * 2.0,
+        (res.y / CONTAINER_DIM_Y) * 2.0
     );
 }
 
 const ndcToRes = (ndc: Vec2) => {
     return new Vec2(
-        (ndc.x * CONTAINER_DIM) / 2.0,
-        (ndc.y * CONTAINER_DIM) / 2.0
+        (ndc.x * CONTAINER_DIM_X) / 2.0,
+        (ndc.y * CONTAINER_DIM_Y) / 2.0
     )
 }
 
