234775dd52812f3dcda7b2ebb30204cea27d55d4
Roger Engelbert <roger@crowdform.co.uk>
2021-12-28 13:59:51 -0300

    Adding sentry error logging to backend

--- backend/Source/Index.ts
+++ backend/Source/Index.ts
@@ -1,9 +1,17 @@
 import { Engine } from "./Core/Engine";
 import { WSClient } from "./Clients/WebSocket";
 import { StrapiClient } from "./Clients/Strapi";
+import * as Sentry from "@sentry/node";
 
 import * as dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
+
+Sentry.init({
+  dsn: "https://0310fb88d0254835be8a3d60b9a17bd6@o1091574.ingest.sentry.io/6108697",
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 const STRAPI_SERVER_HOST = String(process.env.STRAPI_SERVER_HOST);
 const STRAPI_BEARER_TOKEN = String(process.env.STRAPI_BEARER_TOKEN);
