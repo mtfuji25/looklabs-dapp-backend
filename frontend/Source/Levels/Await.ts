e6eba361106ff538e0edbf26648c4c66d58f139a
JoaoMario109 <joao.mario@expertise.dev>
2021-12-29 14:36:50 -0300

    Refactored await level to use async/await operations, avoiding post level's close activity

--- frontend/Source/Levels/Await.ts
+++ frontend/Source/Levels/Await.ts
@@ -1,27 +1,29 @@
 import { Level } from "../Core/Level";
+import { LobbyLevel } from "./Lobby";
 
 // Layers imports
 import { TextLayer } from "../Layers/Await/Text";
 import { MapLayer } from "../Layers/Await/Basemap";
 
-// Web Clients imports
-import { GameStatus, Listener, msgTypes, ServerMsg } from "../Clients/Interfaces";
-import { ScheduledGameParticipant } from "../Clients/Strapi";
-import { LobbyLevel } from "./Lobby";
+// Utils
 import { v4 as uuidv4 } from "uuid";
+
+// Web Clients imports
+import { ScheduledGame } from "../Clients/Strapi";
+import { GameStatus, Listener, msgTypes, ServerMsg } from "../Clients/Interfaces";
 
 // Await level bg color
 const BLACK_BG_COLOR = 0x000;
 
 class AwaitLevel extends Level {
 
-    private listener: Listener;
-    private conListener: Listener;
+    private gameStatusListener: Listener;
+    private connectionListener: Listener;
 
     async onStart(): Promise<void> {
-        // Add msg listener
 
         try {
+            // Request current game status from backend
             const response = await this.context.ws.request({
                 uuid: uuidv4(),
                 type: "request",
@@ -30,8 +32,10 @@ class AwaitLevel extends Level {
                 }
             });
 
+            // Cast the response to corret type
             const content = response.content as GameStatus;
 
+            // If in lobby changes to lobby level
             if (content.gameStatus == "lobby") {
                 await this.context.engine.loadLevel(new LobbyLevel(
                     this.context, "Lobby",
@@ -46,71 +50,103 @@ class AwaitLevel extends Level {
             this.context.close = true;
         }
 
-        this.listener = this.context.ws.addListener("game-status", (msg) => this.onServerMsg(msg));
-
-        this.conListener = this.context.ws.addListener("connection", (ws) => {
-
-            this.context.engine.loadLevel(
-                new AwaitLevel(
-                    this.context, "Await",
-                    {
-                        gameId: this.props.gameId
-                    }
-                )
-            );
-
-            return true;
-        });
+        // Add backend message listeners
+        this.gameStatusListener = this.context.ws.addListener(
+            "game-status",
+            (msg) => this.onGameStatusMessage(msg)
+        );
+        
+        this.connectionListener = this.context.ws.addListener(
+            "connection",
+            (event) => this.onConnectionEvent(event)
+        );
 
         // Sets bg color of main app
         this.context.app.renderer.backgroundColor = BLACK_BG_COLOR;
 
-        this.connectLayers();
-    }
-
-    connectLayers(): void {
+        // Connect background layer
         this.layerStack.pushLayer(new MapLayer(
             this.ecs,
             this.context.app,
             this.context.res
         ));
 
-        this.context.strapi.getGameById(this.props.gameId)
-            .then((game) => {
-                this.layerStack.pushLayer(new TextLayer(
-                    this.ecs,
-                    this.context.app,
-                    this.context,
-                    game
-                ));
-        });
+        // Request current game infos from strapi
+        let game: ScheduledGame;
+
+        try {
+            game = await this.context.strapi.getGameById(this.props.gameId);
+        } catch(e) {
+            console.log(e);
+            this.context.close = true;
+        }
+
+        // Connect infos layer
+        this.layerStack.pushLayer(new TextLayer(
+            this.ecs,
+            this.context.app,
+            this.context,
+            game
+        ));
     }
 
     onUpdate(deltaTime: number) {}
 
     onClose(): void {
-        this.listener.destroy()
-        this.conListener.destroy();
+
+        // Destroy event and message listeners
+        this.gameStatusListener.destroy()
+        this.connectionListener.destroy();
+
+        // Probally redundant
+        // Manually call layerstack destruction
         this.layerStack.destroy();
     }
 
-    onServerMsg(msg: ServerMsg) {
+    // Handles all game-status messages from backend
+    onGameStatusMessage(msg: ServerMsg) {
         let content: GameStatus;
-        console.log("No await", msg)
+
+        // Check the veracity of message type
         if (msg.content.msgType == msgTypes.gameStatus) {
+
+            // Perfom the cast to corret msg type
             content = msg.content as GameStatus;
+
+            // Checks for the interest type of message
             if (content.gameStatus == "lobby") {
+
+                // Changes the level
                 this.context.engine.loadLevel(new LobbyLevel(
                     this.context,
                     "Lobby",
                     {
                         gameId: content.gameId
-                    })
-                )
+                    }
+                ));
             }
         }
-        
+
+        // Doesn't handles the event, allow event propagation
+        // in the event listeners queue
         return false;
+    }
+
+    // Handles all connection events with backend
+    onConnectionEvent(event: Event) {
+        
+        this.context.engine.loadLevel(
+            new AwaitLevel(
+                this.context, "Await",
+                {
+                    gameId: this.props.gameId
+                }
+            )
+        );
+
+        // Handles the event, doesn't allow event propagation
+        // in the event listeners queue
+        return true;
     }
 };
 
