1a6afecfe91b3eb4b4280df742df09487fad063f
Roger Engelbert <roger@crowdform.co.uk>
2022-01-26 15:02:47 -0300

    correcting danger zones in Unstuck behavior
    - Reducing waiting time for behavior

--- backend/Source/Core/Ecs/Strategies/Unstuck.ts
+++ backend/Source/Core/Ecs/Strategies/Unstuck.ts
@@ -9,18 +9,20 @@ type DangerZone = {
     target:Vec2
 };
 
-const MAX_TIME_STUCK = 40000;
+const MAX_TIME_STUCK = 30000;
+
 
 const dangerZones:DangerZone[] = [
+    { quadrant: [new Vec2(104,74), new Vec2(99,79)], target: new Vec2(0.4741,-0.3190)},
+    { quadrant: [new Vec2(108,82), new Vec2(99,88)], target: new Vec2(0.4741,-0.4569)},
+    { quadrant: [new Vec2(114,67), new Vec2(99,72),], target: new Vec2(0.7155,-0.0948)},
+    { quadrant: [new Vec2(105,72), new Vec2(91,82)], target: new Vec2(0.5086,-0.3190)},
     { quadrant: [new Vec2(114,32), new Vec2(82,41)], target: new Vec2(0.2845,0.3362)},
     { quadrant: [new Vec2(114,42), new Vec2(82,49)], target: new Vec2(0.3362,0.1983)},
     { quadrant: [new Vec2(114,50), new Vec2(83,55)], target: new Vec2(0.6638,-0.0259)},
     { quadrant: [new Vec2(114,65), new Vec2(91,69)], target:  new Vec2(0.5431,-0.0603)},
     { quadrant: [new Vec2(114,72), new Vec2(91,72)], target:  new Vec2(0.6466,-0.3190),},
-    { quadrant: [new Vec2(114,76), new Vec2(105,80)], target: new Vec2(0.8707,-0.2500)},
-    { quadrant: [new Vec2(114,65), new Vec2(91,69)], target:  new Vec2(0.5431,-0.0603)},
-    { quadrant: [new Vec2(114,72), new Vec2(91,72)], target:  new Vec2(0.6466,-0.3190)},
-    { quadrant: [new Vec2(114,76), new Vec2(105,80)], target: new Vec2(0.8707,-0.2500)},
+    { quadrant: [new Vec2(113,76), new Vec2(105,80)], target:  new Vec2(0.8879,-0.0776)},
     { quadrant: [new Vec2(51,32), new Vec2(45,39),], target: new Vec2(-0.1466,0.2500)},
     { quadrant: [new Vec2(71,32), new Vec2(63,39),], target: new Vec2(0.2845,0.2672)},
     { quadrant: [new Vec2(72,78), new Vec2(65,89),], target: new Vec2(0.3362,-0.4397)},
@@ -29,10 +31,10 @@ const dangerZones:DangerZone[] = [
     { quadrant: [new Vec2(51,78), new Vec2(45,89)], target: new Vec2(-0.2500,-0.4224)},
     { quadrant: [new Vec2(35,74),  new Vec2(1,79)], target: new Vec2(-0.6638,-0.1983)},
     { quadrant: [new Vec2(36,80), new Vec2(1,87)], target: new Vec2(-0.2155,-0.4569)},
-    { quadrant: [new Vec2(79,49), new Vec2(69,57)], target: new Vec2(0.3879,0.2672)},
-    { quadrant: [new Vec2(78,62), new Vec2(69,70)], target: new Vec2(0.3879,-0.3190)},
-    { quadrant: [new Vec2(47,48), new Vec2(39,57)], target: new Vec2(-0.3534,0.2328)},
-    { quadrant: [new Vec2(48,62), new Vec2(39,70)], target: new Vec2(-0.3534,-0.2845)},
+    { quadrant: [new Vec2(78,49), new Vec2(69,57)], target: new Vec2(0.2328,0.2845)},
+    { quadrant: [new Vec2(78,62), new Vec2(69,70)], target: new Vec2(0.2155,-0.3362)},
+    { quadrant: [new Vec2(47,48), new Vec2(39,57)], target: new Vec2(-0.2155,0.2672)},
+    { quadrant: [new Vec2(48,62), new Vec2(39,71)], target: new Vec2(-0.1983,-0.3190)}
 ];
 
 const strategy_Unstuck = (entity:Entity, grid:Grid, target?:Entity):void => {
@@ -61,7 +63,7 @@ const strategy_Unstuck = (entity:Entity, grid:Grid, target?:Entity):void => {
 
 const isPlayerStuck = (dynamic:DynamicEntity):boolean => {
     
-    if (entitiesAlive > 10) return false;
+    if (entitiesAlive > 20) return false;
     const strategy = dynamic.entity.getStrategy();
 
     // if player is colliding with another player, disregard
